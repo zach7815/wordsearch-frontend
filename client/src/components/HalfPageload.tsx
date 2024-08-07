2648940046730f11a81a90a5459974b61baddd17
zachariahkozlowski-best <mcfeegles@gmail.com>
2024-08-07 14:52:10 +0800

    feat: added production message about loading time

--- client/src/components/HalfPageload.tsx
+++ client/src/components/HalfPageload.tsx
@@ -1,14 +1,15 @@
 const HalfpageSpinner = () => {
   return (
-    <>
+
       <div className="w-1/2 h-full flex items-center  flex-col content-between justify-center overflow-hidden absolute bg-white  ">
         <svg
           className=" animate-spin rounded-full border-4 border-blue-500 border-t-transparent h-8 w-8 "
           viewBox="0 0 24 24"
         ></svg>
         <p> Processing...</p>
+        <p>  Please note this project is ran on a free instance so the loading time can take upto 50 secs or more</p>
       </div>
-    </>
+
   );
 };
 
